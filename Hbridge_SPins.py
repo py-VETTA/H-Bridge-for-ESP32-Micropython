f3720eb60072ed82b58752afa6ab588c54acf911
py-VETTA <gcpivetta@gmail.com>
2026-09-04 18:31:32 -0300

    Create Hbridge_SPins.py
    Biblioteca para conexão de Ponte H com ESP32 com 2 pinos PWM especiais para mudança de velocidade.

---
+++ Hbridge_SPins.py
@@ -0,0 +1,148 @@
+from machine import Pin, PWM
+
+# - Ponte H conectada ao ESP32 com pinos especiais para o PWM.
+
+class Hbridge_Speed_Pins:
+    
+
+# - Denomina os pinos quando chama a classe sendo, em ordem: n1; n2; n3; n4; pwm1; pwm2.
+#  
+# Exemplo: Hbridge_Speed_Pins(12, 14, 27, 26, 25, 33)
+    
+    def __init__(self, n1, n2, n3, n4, pwm1, pwm2):
+        self.n1 = Pin(n1, Pin.OUT)
+        self.n2 = Pin(n2, Pin.OUT)
+        self.n3 = Pin(n3, Pin.OUT)
+        self.n4 = Pin(n4, Pin.OUT)
+        self.pwm1 = PWM(Pin(pwm1), freq=5000, duty=0)
+        self.pwm2 = PWM(Pin(pwm2), freq=5000, duty=0)
+        self.min_speed = 0
+        
+
+    
+# - Função para o motor ligar, girar em sentindo horário ou anti-horário, e alterar sua velocidade;
+#
+# - Para chama-lá, precisa fornecer os valores de Nome do Motor em string (Direito ['right'] ou ['r'],
+#  Esquerdo: ['left'] ou ['l'], ou Todos: ['all'] ou ['a']) * , qual o Sentido de Giro em string (Horário ['cw'] ou
+#  Antihorário ['ccw']) * , e qual a Porcentagem da Velocidade de Rotação em inteiros (0 a 100);
+#
+#   * não há problemas no aparecimento de letras MAIÚSCULAS nas strings.
+#
+# Exemplo: 
+#               Hbridge_Speed_Pins.Run("Right", "CCW", 70)
+
+    def Run(self, motor, direction, speed_percent):
+
+        if motor.lower() in ("left", "l"):
+
+            self.percent_to_pwm(speed_percent)
+            self.pwm1.duty(self.speed)
+
+            if direction.lower() == "ccw":
+                self.n2.on()
+                self.n1.off()
+            elif direction.lower() == "cw":
+                self.n2.off()
+                self.n1.on()
+            else:
+                print(f"{direction} - valor da Direção não válido")
+                
+        elif motor.lower() in ("right", "r"):
+
+            self.percent_to_pwm(speed_percent)
+            self.pwm2.duty(self.speed)
+
+            if direction.lower() == "cw":
+                self.n4.on()
+                self.n3.off()
+            elif direction.lower() == "ccw":
+                self.n4.off()
+                self.n3.on()
+            else:
+                print(f"{direction} - valor da Direção não válido")
+
+        elif motor.lower() in ("all", "a"):
+
+            self.percent_to_pwm(speed_percent)
+            self.pwm1.duty(self.speed)
+            self.pwm2.duty(self.speed)
+
+            if direction.lower() == "cw":
+                self.n2.off()
+                self.n1.on()
+                self.n4.on()
+                self.n3.off()
+            elif direction.lower() == "ccw":
+                self.n2.on()
+                self.n1.off()
+                self.n4.off()
+                self.n3.on()
+            else:
+                print(f"{direction} - valor da Direção não válido")
+
+        else:
+            print(f"{motor} - valor do Motor não válido")
+            self.n2.off()
+            self.n1.off()
+            self.n4.off()
+            self.n3.off()
+    
+    
+
+
+# - Para o motor quando chamada com a string do Nome do Motor (Direito: ['right'] ou ['r'],
+#  Esquerdo: ['left'] ou ['l'], ou Todos: ['all'] ou ['a']) * ;
+#
+#   * não há problemas no aparecimento de letras MAIÚSCULAS nas strings.
+#
+# Exemplo:
+#               Hbridge_Speed_Pins.Stop('all')
+
+    def Stop(self, motor):
+        
+        if motor.lower() in ("left", "l"):
+            self.n1.off()
+            self.n2.off()
+
+        elif motor.lower() in ("right", "r"):
+            self.n3.off()
+            self.n4.off()
+
+        elif motor.lower() in ("all", "a"):
+            self.n1.off()
+            self.n2.off()
+            self.n3.off()
+            self.n4.off()
+
+        else:
+            print(f"{motor} - valor do Motor não válido")
+            self.n2.off()
+            self.n1.off()
+            self.n4.off()
+            self.n3.off()
+             
+
+
+
+
+# - Utiliza-se para determinar o valor de velocidade mínimo que o motor suporta.
+#   
+# Exemplo:
+#               Hbridge_Speed_Pins.min_speed_config(200)
+#
+# ---quando o valor de velocidade escolhido for 1, o valor do duty do PWM será 200---
+
+    def min_speed_config(self, min_speed_value):
+        self.min_speed = min(max(min_speed_value, 0), 1023)
+
+
+#
+# - Faz a converção da velocidade em porcentagem para escala 10 bits utilizando o valor mínimo de velocidade.
+#
+
+    def percent_to_pwm(self, speed_percent):
+        sp = max(min(speed_percent, 100), 0)
+        if sp <= 0:
+            self.speed = 0
+        else:
+            self.speed =  int(self.min_speed + (sp - 1) * (1023 - self.min_speed) / 99)
